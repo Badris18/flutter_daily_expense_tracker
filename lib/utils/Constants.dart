c036199174aa92ca6670b11e844d03a1afc1cd09
Badris18 <badrivk1008@gmail.com>
2025-01-20 18:36:01 +0530

    Update Constants.dart

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -1,11 +1,18 @@
 class Constants  {
 
-  static const String BASE_URL = "http:///192.168.0.113:8000/api";
+  static const String BASE_URL = "http://192.168.1.4:8000/api";
 
-  static const String ALL_TRANSACTION_ROUTE = "/transactions";
+  static const String ALL_TRANSACTION_ROUTE = "/v2/transactions";
 
-  static const String INCOME_TRANSACTION_ROUTE = "/transactions?type=income";
+  static const String INCOME_TRANSACTION_ROUTE = "/v2/transactions?type=income";
 
-  static const String EXPENSE_TRANSACTION_ROUTE = "/transactions?type=expense";
+  static const String EXPENSE_TRANSACTION_ROUTE = "/v2/transactions?type=expense";
+
+  static const String ADD_TRANSACTION = '/v2/transactions';
+
+  // Route for a single transaction. Replace {id} with the actual transaction ID.
+  static const String SINGLE_TRANSACTION_ROUTE = "/v2/transactions/{id}";
+
+  static const String UPDATE_TRANSACTION_ROUTE = "/v2/transactions/{id}";
 
 }
